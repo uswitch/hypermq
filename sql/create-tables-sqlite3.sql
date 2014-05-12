ef70c1002577285baf4285ea8de9a5eb6754ae89
Russell <russell@russelldunphy.com>
2014-05-12 14:53:50 +0100

    Set env to production in production

---
+++ sql/create-tables-sqlite3.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS message (
+  id INTEGER PRIMARY KEY,
+  uuid VARCHAR(36),
+  queue VARCHAR(255) NOT NULL,
+  producer VARCHAR(255),
+  body BLOB,
+  created BIGINT NOT NULL
+);
+CREATE INDEX queue_idx ON message (queue);
+CREATE INDEX uuid_idx ON message (uuid);
+
+CREATE TABLE IF NOT EXISTS acknowledgement (
+  queue varchar(255) NOT NULL,
+  client VARCHAR(255) NOT NULL,
+  message BIGINT NOT NULL,
+  created BIGINT NOT NULL,
+  PRIMARY KEY (queue, client, message)
+);
+
